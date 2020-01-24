31df786cf229a2485bec9524d51e19d97e1ce38e
Calvin Bruch <calvinbcoding@gmail.com>
2020-01-24 01:52:44 -0700

    imported components into app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Route, Switch, withRouter } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header/Header';
+import NavBar from './components/NavBar/NavBar';
+import Main from './components/Main/Main';
+import Register from './components/Auth/Register';
+import Login from './components/Auth/Login';
+import Users from './components/Users/Users';
+import UserProfile from './components/Users/UserProfile';
+import Games from './components/Games/Games';
+import Chat from './components/Chat/Chat';
+import Footer from './components/Footer/Footer';
 
-function App() {
-  return (
-    <div className="App">
-    
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      
+      </div>
+    );
+  }
 }
 
 export default App;
